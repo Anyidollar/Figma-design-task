39153ad07a6131af02b5bab5478a57a4cb214f46
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-07-02 19:26:14 +0100

    refactored navbar component code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Session1 from "./components/pages/Session1";
+import Session4 from "./components/pages/Session4";
+import Session5 from "./components/pages/Session5";
+import Session6 from "./components/pages/Session6";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Session1 />
+      <Session4 />
+      <Session5 />
+      <Session6 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
